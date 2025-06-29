79fb46e55c896cdefc00daee3fa268ac4d325be1
VisaganSankar <visagansankar83@gmail.com>
2025-06-29 21:31:08 +0530

    completed

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,9 +1,11 @@
+// backend/models/Task.js
+
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
   title: String,
   done: { type: Boolean, default: false },
-  userId: String 
-});
+  userId: String,
+}, { timestamps: true }); // ✅ enables createdAt & updatedAt fields
 
 module.exports = mongoose.model('Task', TaskSchema);
