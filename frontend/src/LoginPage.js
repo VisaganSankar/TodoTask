f14941189bc19ff8856dddee908a208c7ba24ab5
VisaganSankar <visagansankar83@gmail.com>
2025-06-29 19:05:11 +0530

    removed the icon

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -86,7 +86,7 @@ function LoginPage({ setUser }) {
 
       <hr style={{ margin: '30px auto', width: '60%', borderColor: '#444' }} />
 
-      <button className="save-btn" onClick={handleGoogleLogin}> Sign in with Google</button>
+      <button className="save-btn" onClick={handleGoogleLogin}> Sign in with google</button>
 
       <p style={{ marginTop: '20px' }}>
         Don’t have an account? <a href="/register">Register</a>
